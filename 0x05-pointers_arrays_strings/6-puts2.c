1b1e09cd930565efa0641c537f8d9c906c38e97c
SelamFesha <selamfesha21@gmail.com>
2023-07-23 21:27:02 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,22 @@
-/*
- * File: 6-puts2.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
- * puts2 - Prints one char out of two of a string.
- * @str: The string containing characters.
+ * puts2 - prints every other character
+ * @str: string
+ *
+ * Return: nothing
  */
+
 void puts2(char *str)
 {
-	int index = 0, len = 0;
+	int i = 0;
 
-	while (str[index++])
-		len++;
-
-	for (index = 0; index < len; index += 2)
-		_putchar(str[index]);
-
-	_putchar('\n');
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+			putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
 }
