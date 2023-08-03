bc3e228bbb750ce6fba43cfe057501344e1a34cb
SelamFesha <selamfesha21@gmail.com>
2023-08-03 11:35:28 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,13 @@
+/*
+ * File: 1-print_rev_recursion.c
+ * Auth: sam
+ */
+
+#include "main.h"
+
 /**
- * _print_rev_recursion - prints a string in reverse
- * @s: An input string to print
- *
- * Return: void
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
 void _print_rev_recursion(char *s)
 {
